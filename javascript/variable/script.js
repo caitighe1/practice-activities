3f85d5514e28cc353cc806a3ed00b61ded1e12b7
Caitlin Tighe <caitighe@gmail.com>
2021-02-09 14:57:15 -0500

    variable activity completed

---
+++ javascript/variable/script.js
@@ -0,0 +1,18 @@
+// Write Your JavaScript Code Here
+var studentName;
+var petName;
+var funFact;
+
+var studentName = "Caitlin";
+var petName = "Nina";
+var funFact = "I have two sisters";
+
+studentName = "Ariana";
+petName = "Sammy";
+funFact = "I have a brother";
+
+console.log("My name is " + studentName);
+console.log("My pets name is " + petName);
+console.log("Fun fact: " + funFact);
+
+
